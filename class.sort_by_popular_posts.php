4dbe242fd0cc5d08c4d625958eae0ec5367ac0a4
Takuho Nakano <takotakot@users.noreply.github.com>
2016-12-23 22:33:43 +0900

    Version 0.0.1

---
+++ class.sort_by_popular_posts.php
@@ -0,0 +1,258 @@
+<?php
+
+class SortByPopularPosts {
+
+	private static $initiated = false;
+
+	private static $days = 7;
+
+	private static $cron_time = '4:40:00';
+
+	const PV_TABLE = 'sbpp_wppp';
+
+	const PPS_TABLE = 'popularpostssummary';
+
+	public static function init() {
+		if ( ! self::$initiated ) {
+			self::init_hooks();
+		}
+	}
+
+	/**
+	 * Initializes WordPress hooks
+	 */
+	private static function init_hooks() {
+		self::$initiated = true;
+	}
+
+	/**
+	 * Run when this plugin is activated
+	 */
+	public static function activation() {
+		self::create_sbpp_wppp_table();
+		self::update_sbpp_wppp();
+	}
+
+	/**
+	 * Create table
+	 */
+	public static function create_sbpp_wppp_table() {
+		global $wpdb;
+		$table_sbpp_wppp = $wpdb->prefix . self::PV_TABLE;
+		$charset_collate = $wpdb->get_charset_collate();
+		
+		require_once ( ABSPATH . '/wp-admin/includes/upgrade.php' );
+		$sql = "
+			CREATE TABLE `{$table_sbpp_wppp}` (
+			 `id` bigint(20) NOT NULL AUTO_INCREMENT,
+			 `postid` bigint(20) NOT NULL,
+			 `days` int(11) NOT NULL,
+			 `pageviews` bigint(20) NOT NULL,
+			 PRIMARY KEY (`id`),
+			 UNIQUE KEY `postid_days` (`postid`,`days`),
+			 KEY `days_pageviews` (`days`,`pageviews`)
+			) ENGINE=InnoDB {$charset_collate};";
+		dbDelta( $sql );
+	}
+
+	/**
+	 * Update table
+	 */
+	public static function update_sbpp_wppp() {
+		global $wpdb;
+		
+		// set table name
+		$prefix = $wpdb->prefix;
+		
+		$table_sbpp_wppp = $prefix . self::PV_TABLE;
+		$table_wppp_pps = $prefix . self::PPS_TABLE;
+		$table_posts = $prefix . 'posts';
+		
+		// update old
+		$sql_update = sprintf( 
+			"
+			UPDATE {$table_sbpp_wppp} AS sbpp
+			SET
+			 pageviews = (
+			  SELECT COALESCE(sum(pps.pageviews), 0)
+			   FROM {$table_wppp_pps} AS pps
+			   WHERE
+			     sbpp.postid = pps.postid
+			    AND
+			     pps.view_date BETWEEN (CURRENT_DATE - INTERVAL %d day) AND (CURRENT_DATE - INTERVAL 1 day)
+			    AND
+			     sbpp.days = %d
+			   GROUP BY pps.postid
+			  );
+			", 
+			self::$days, 
+			self::$days );
+		
+		// update old zero
+		$sql_update_zero = sprintf( 
+			"
+			UPDATE {$table_sbpp_wppp} AS sbpp
+			SET
+			 pageviews = 0
+			WHERE
+			 sbpp.postid NOT IN (
+			  SELECT pps.postid
+			   FROM {$table_wppp_pps} AS pps
+			   WHERE
+			     sbpp.postid = pps.postid
+			    AND
+			     pps.view_date BETWEEN (CURRENT_DATE - INTERVAL %d day) AND (CURRENT_DATE - INTERVAL 1 day)
+			    AND
+			     sbpp.days = %d
+			  );
+			", 
+			self::$days, 
+			self::$days );
+		
+		// insert new
+		$sql_insert = sprintf( 
+			"
+			INSERT INTO {$table_sbpp_wppp}(postid, days, pageviews)
+			 SELECT posts.ID, %d, COALESCE(sum(pps.pageviews), 0)
+			  FROM {$table_posts} AS posts, {$table_wppp_pps} AS pps
+			  WHERE
+			    posts.post_type = 'post'
+			   AND
+			     (posts.post_status = 'publish'
+			    OR posts.post_status = 'private')
+			   AND
+			    pps.view_date BETWEEN (CURRENT_DATE - INTERVAL %d day) AND (CURRENT_DATE - INTERVAL 1 day)
+			   AND
+			    posts.ID = pps.postid
+			   AND
+			    posts.ID NOT IN (SELECT postid FROM {$table_sbpp_wppp})
+			  GROUP BY posts.ID;
+			", 
+			self::$days, 
+			self::$days );
+		
+		// insert new zero
+		$sql_insert_zero = sprintf( 
+			"
+			INSERT INTO {$table_sbpp_wppp} (postid, days, pageviews)
+			 SELECT posts.ID, %d, 0
+			 FROM {$table_posts} AS posts
+			  WHERE
+			    posts.post_type = 'post'
+			   AND
+			     (posts.post_status = 'publish'
+			    OR posts.post_status = 'private')
+			   AND
+			    posts.ID NOT IN (SELECT postid FROM {$table_sbpp_wppp});
+			", 
+			self::$days );
+		
+		// remove deleted
+		$sql_delete = sprintf( 
+			"
+			DELETE sbpp
+			 FROM {$table_sbpp_wppp} AS sbpp
+			 WHERE
+			 sbpp.postid NOT IN (
+			  SELECT posts.ID
+			   FROM {$table_posts} AS posts
+			   WHERE
+			     posts.post_type = 'post'
+			    AND
+			      (posts.post_status = 'publish'
+			     OR posts.post_status = 'private')
+			  );
+			" );
+		
+		$wpdb->query( $sql_update );
+		$wpdb->query( $sql_update_zero );
+		$wpdb->query( $sql_insert );
+		$wpdb->query( $sql_insert_zero );
+		$wpdb->query( $sql_delete );
+	}
+
+	private static function need_pageviews( $query ) {
+		if ( is_admin() || ! $query->is_main_query() )
+			return false;
+		
+		if ( $query->is_category() || $query->is_tag() )
+			return true;
+		
+		return false;
+	}
+
+	private static function need_posts_order( $query ) {
+		if ( is_admin() || ! $query->is_main_query() )
+			return false;
+		
+		if ( $query->is_category() || $query->is_tag() )
+			return true;
+		
+		return false;
+	}
+
+	public static function get_cron_time() {
+		$cron_time = self::$cron_time;
+		$cur_time = current_time( 'timestamp' );
+		$time_zone_diff = intval( ( $cur_time - time() ) / 1800 ) * 1800;
+		
+		$time_next_str = date( sprintf( 'Y-m-d %s', $cron_time ), $cur_time );
+		
+		return strtotime( $time_next_str ) - $time_zone_diff;
+	}
+
+	public static function posts_join( $join, $query ) {
+		if ( ! ( self::need_pageviews( $query ) || self::need_posts_order( $query ) ) )
+			return $join;
+		
+		global $wpdb;
+		$pv_table = $wpdb->prefix . self::PV_TABLE;
+		
+		$join .= " LEFT JOIN {$pv_table} ON {$wpdb->posts}.ID = {$pv_table}.postid ";
+		return $join;
+	}
+
+	public static function posts_orderby( $orderby, $query ) {
+		if ( ! self::need_posts_order( $query ) )
+			return $orderby;
+		
+		global $wpdb;
+		$pv_table = $wpdb->prefix . self::PV_TABLE;
+		
+		$orderby = "COALESCE(SUM({$pv_table}.pageviews), 0) DESC, {$wpdb->posts}.post_date DESC";
+		return $orderby;
+	}
+
+	public static function posts_where( $where, $query ) {
+		return $where;
+	}
+
+	public static function posts_groupby( $groupby, $query ) {
+		if ( ! ( self::need_pageviews( $query ) || self::need_posts_order( $query ) ) )
+			return $groupby;
+		
+		global $wpdb;
+		$mygroupby = "{$wpdb->posts}.ID";
+		
+		if ( preg_match( "/$mygroupby/", $groupby ) ) {
+			return $groupby;
+		}
+		if ( ! strlen( trim( $groupby ) ) ) {
+			return $mygroupby;
+		}
+		
+		$groupby = $groupby . ', ' . $mygroupby;
+		return $groupby;
+	}
+
+	public static function posts_fields( $fields, $query ) {
+		if ( ! self::need_pageviews( $query ) )
+			return $fields;
+		
+		global $wpdb;
+		$pv_table = $wpdb->prefix . self::PV_TABLE;
+		
+		$fields .= ", sum({$pv_table}.pageviews) AS pageviews";
+		return $fields;
+	}
+}
---
+++ sort_by_popular_posts.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package SortByPopularPosts
+ */
+/*
+ * Plugin Name: SortByPopularPosts
+ * Plugin URI: http://example.com/
+ * Description: Display posts sort by popular posts.
+ * Version: 0.0.1
+ * Author: takotakot
+ * Author URI: http://example.com/
+ * License: MIT/X
+ * Text Domain: sortbypopularposts
+ */
+
+// Make sure we don't expose any info if called directly
+if ( ! function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit();
+}
+
+define( 'SORTBYPOPULARPOSTS_VERSION', '0.0.1' );
+define( 'SORTBYPOPULARPOSTS__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'SORTBYPOPULARPOSTS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+require_once ( SORTBYPOPULARPOSTS__PLUGIN_DIR . 'class.sort_by_popular_posts.php' );
+
+// $sbpp_instance = new SortByPopularPosts();
+
+add_filter('posts_join', array( 'SortByPopularPosts', 'posts_join' ), 10, 2);
+add_filter('posts_orderby', array( 'SortByPopularPosts', 'posts_orderby' ), 10, 2);
+add_filter('posts_where', array( 'SortByPopularPosts', 'posts_where' ), 10, 2);
+add_filter('posts_groupby', array( 'SortByPopularPosts', 'posts_groupby' ), 10, 2);
+add_filter('posts_fields', array( 'SortByPopularPosts', 'posts_fields' ), 10, 2);
+
+add_action( 'init', array( 'SortByPopularPosts', 'init' ) );
+add_action( 'daily_sort_by_popular_posts_update', 'daily_sort_by_popular_posts_update_action' );
+
+// activation
+if ( function_exists( 'register_activation_hook' ) ) {
+	register_activation_hook( __FILE__, 'register_sort_by_popular_posts_activation' );
+	register_activation_hook( __FILE__, 'register_sort_by_popular_posts_cron' );
+}
+
+function register_sort_by_popular_posts_activation() {
+	\SortByPopularPosts::activation();
+}
+
+function register_sort_by_popular_posts_cron() {
+	if ( ! wp_next_scheduled( 'daily_sort_by_popular_posts_update' ) ) {
+		// TODO: justify time
+		wp_schedule_event( \SortByPopularPosts::get_cron_time(), 'daily', 'daily_sort_by_popular_posts_update' );
+	}
+}
+
+function remove_sort_by_popular_posts_cron() {
+	if ( wp_next_scheduled( 'daily_sort_by_popular_posts_update' ) ) {
+		wp_clear_scheduled_hook( 'daily_sort_by_popular_posts_update' );
+	}
+}
+
+function daily_sort_by_popular_posts_update_action() {
+	\SortByPopularPosts::update_sbpp_wppp();
+}
+
+// deactivation
+if ( function_exists( 'register_deactivation_hook' ) ) {
+	register_deactivation_hook( __FILE__, 'remove_sort_by_popular_posts_cron' );
+}
