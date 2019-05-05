11239e7aa5818d1f3c95d6bd6f0b0f3d530823a1
Takuho Nakano <takotakot@users.noreply.github.com>
2019-05-05 10:26:55 +0900

    Updage version

--- sort_by_popular_posts.php
+++ sort_by_popular_posts.php
@@ -6,9 +6,9 @@
  * Plugin Name: SortByPopularPosts
  * Plugin URI: https://github.com/takotakot/sort_by_popular_posts
  * Description: Display posts sort by popular posts.
- * Version: 0.0.7
+ * Version: 0.0.8
  * Author: takotakot
- * Author URI: http://example.com/
+ * Author URI: https://github.com/takotakot/
  * License: MIT/X
  * Text Domain: sortbypopularposts
  */
@@ -19,7 +19,7 @@ if ( ! function_exists( 'add_action' ) ) {
 	exit();
 }
 
-define( 'SORTBYPOPULARPOSTS_VERSION', '0.0.6' );
+define( 'SORTBYPOPULARPOSTS_VERSION', '0.0.8' );
 define( 'SORTBYPOPULARPOSTS__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SORTBYPOPULARPOSTS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
